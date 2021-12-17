e8f733992e8cd6f361c86d07b74fe6ec64909099
Sovannra <sovannrakong@gmail.com>
2021-12-17 13:29:32 +0700

    first commit

---
+++ Example/Pods/Target Support Files/BJTextView/BJTextView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BJTextView : NSObject
+@end
+@implementation PodsDummy_BJTextView
+@end
---
+++ Example/Pods/Target Support Files/Pods-BJTextView_Example/Pods-BJTextView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BJTextView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BJTextView_Example
+@end
